2c7428cdfcd0e7704404134b8becbd792cc3caa8
Pratham Bhatnagar <84272138+pratham-bhatnagar@users.noreply.github.com>
2021-10-24 23:58:00 +0530

    README.md update

--- Task-1/moneyProblem.js
+++ Task-1/moneyProblem.js
@@ -1,6 +1,6 @@
-let denomination = [2000,500,200,100,50,20,10,5,2,1]
-let amount = 98731;
-let notes = [];
+const denomination = [2000,500,200,100,50,20,10,5,2,1]
+const amount = 98731;
+const notes = [];
 
 for(let i=0; i<denomination.length;i++)
 {
@@ -15,7 +15,7 @@ for(let i=0; i<denomination.length;i++)
     }
     
 }
-let index = 0;
+const index = 0;
 while(index<denomination.length){
     console.log(denomination[index],' - ',notes[index])
     index++
